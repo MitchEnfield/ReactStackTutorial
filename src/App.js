3b9576c67e6592d74da954a033b60c26d48ea342
Mitchell Enfield <Menfield@driversalert.com>
2017-09-14 13:35:29 -0400

    Added email verification page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route} from 'react-router-dom'
 import HomePage from './components/Pages/HomePage'
 import LoginPage from './components/Pages/LoginPage'
 import SignupPage from './components/Pages/SignupPage'
+import ConfirmationPage from './components/Pages/ConfirmationPage'
 import DashboardPage from './components/Pages/DashboardPage'
 import UserRoute from './components/Routes/UserRoute'
 import GuestRoute from './components/Routes/GuestRoute'
@@ -12,6 +13,7 @@ const App = ({location}) => {
   return (
     <div className='ui container'>
       <Route location={location} path='/' exact component={HomePage} />
+      <Route location={location} path='/confirmation/:token' exact component={ConfirmationPage} />
       <GuestRoute location={location} path='/Login' exact component={LoginPage} />
       <GuestRoute location={location} path='/SignUp' exact component={SignupPage} />
       <UserRoute location={location} path='/Dashboard' exact component={DashboardPage} />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import decode from 'jwt-decode';
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -18,7 +19,12 @@ const store = createStore(
 )
 
 if(localStorage.bookwormJWT){
-  const user = { token: localStorage.bookwormJWT };
+  const payload = decode(localStorage.bookwormJWT);
+  const user = {
+    token: localStorage.bookwormJWT,
+    email: payload.email,
+    confirmed: payload.confirmed
+  };
   store.dispatch(userLoggedIn(user));
 }
 
